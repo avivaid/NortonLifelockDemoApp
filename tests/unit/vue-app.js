4339aaf22266b1e5549ea7e6ad03735ab761897b
Aviral Vaid <avi.aviralvaid@gmail.com>
2021-08-03 18:52:04 -0700

    Added mock data for testing

--- tests/unit/vue-app.js
+++ tests/unit/vue-app.js
@@ -21,7 +21,7 @@ const data = [
     "SurfaceTempK": "",
     "AgeGyr": "",
     "DiscoveryMethod": "transit",
-    "DiscoveryYear": 2010,
+    "DiscoveryYear": 2011,
     "LastUpdated": "13/07/15",
     "RightAscension": "19 00 03.14",
     "Declination": "+40 13 14.7",
@@ -99,7 +99,7 @@ const data = [
     "SurfaceTempK": "",
     "AgeGyr": "",
     "DiscoveryMethod": "transit",
-    "DiscoveryYear": 2013,
+    "DiscoveryYear": 2012,
     "LastUpdated": "13/07/15",
     "RightAscension": "19 00 03.14",
     "Declination": "+40 13 14.7",
@@ -125,7 +125,7 @@ const data = [
     "SurfaceTempK": "",
     "AgeGyr": "",
     "DiscoveryMethod": "transit",
-    "DiscoveryYear": 2014,
+    "DiscoveryYear": 2011,
     "LastUpdated": "13/07/15",
     "RightAscension": "19 00 03.14",
     "Declination": "+40 13 14.7",
@@ -188,12 +188,121 @@ const data = [
     "HostStarTempK": 3584000,
     "HostStarAgeGyr": ""
   },
+  {
+    "PlanetIdentifier": "KOI-1843.03",
+    "TypeFlag": 1,
+    "PlanetaryMassJpt": 0.0014,
+    "RadiusJpt": 1.001,
+    "PeriodDays": 0.1768913,
+    "SemiMajorAxisAU": 0.0048,
+    "Eccentricity": "",
+    "PeriastronDeg": "",
+    "LongitudeDeg": "",
+    "AscendingNodeDeg": "",
+    "InclinationDeg": 72,
+    "SurfaceTempK": "",
+    "AgeGyr": "",
+    "DiscoveryMethod": "transit",
+    "DiscoveryYear": 2012,
+    "LastUpdated": "13/07/15",
+    "RightAscension": "19 00 03.14",
+    "Declination": "+40 13 14.7",
+    "DistFromSunParsec": "",
+    "HostStarMassSlrMass": 0.46,
+    "HostStarRadiusSlrRad": 0.45,
+    "HostStarMetallicity": 0,
+    "HostStarTempK": 3584000,
+    "HostStarAgeGyr": ""
+  },
+
+  {
+    "PlanetIdentifier": "KOI-1843.03",
+    "TypeFlag": 1,
+    "PlanetaryMassJpt": 0.0014,
+    "RadiusJpt": 5.4,
+    "PeriodDays": 0.1768913,
+    "SemiMajorAxisAU": 0.0048,
+    "Eccentricity": "",
+    "PeriastronDeg": "",
+    "LongitudeDeg": "",
+    "AscendingNodeDeg": "",
+    "InclinationDeg": 72,
+    "SurfaceTempK": "",
+    "AgeGyr": "",
+    "DiscoveryMethod": "transit",
+    "DiscoveryYear": 2012,
+    "LastUpdated": "13/07/15",
+    "RightAscension": "19 00 03.14",
+    "Declination": "+40 13 14.7",
+    "DistFromSunParsec": "",
+    "HostStarMassSlrMass": 0.46,
+    "HostStarRadiusSlrRad": 0.45,
+    "HostStarMetallicity": 0,
+    "HostStarTempK": 3584000,
+    "HostStarAgeGyr": ""
+  },
+  {
+    "PlanetIdentifier": "KOI-1843.03",
+    "TypeFlag": 1,
+    "PlanetaryMassJpt": 0.0014,
+    "RadiusJpt": 1.001,
+    "PeriodDays": 0.1768913,
+    "SemiMajorAxisAU": 0.0048,
+    "Eccentricity": "",
+    "PeriastronDeg": "",
+    "LongitudeDeg": "",
+    "AscendingNodeDeg": "",
+    "InclinationDeg": 72,
+    "SurfaceTempK": "",
+    "AgeGyr": "",
+    "DiscoveryMethod": "transit",
+    "DiscoveryYear": 2011,
+    "LastUpdated": "13/07/15",
+    "RightAscension": "19 00 03.14",
+    "Declination": "+40 13 14.7",
+    "DistFromSunParsec": "",
+    "HostStarMassSlrMass": 0.46,
+    "HostStarRadiusSlrRad": 0.45,
+    "HostStarMetallicity": 0,
+    "HostStarTempK": 3584000,
+    "HostStarAgeGyr": ""
+  },
+
+  {
+    "PlanetIdentifier": "KOI-1843.03",
+    "TypeFlag": 1,
+    "PlanetaryMassJpt": 0.0014,
+    "RadiusJpt": 5.4,
+    "PeriodDays": 0.1768913,
+    "SemiMajorAxisAU": 0.0048,
+    "Eccentricity": "",
+    "PeriastronDeg": "",
+    "LongitudeDeg": "",
+    "AscendingNodeDeg": "",
+    "InclinationDeg": 72,
+    "SurfaceTempK": "",
+    "AgeGyr": "",
+    "DiscoveryMethod": "transit",
+    "DiscoveryYear": 2011,
+    "LastUpdated": "13/07/15",
+    "RightAscension": "19 00 03.14",
+    "Declination": "+40 13 14.7",
+    "DistFromSunParsec": "",
+    "HostStarMassSlrMass": 0.46,
+    "HostStarRadiusSlrRad": 0.45,
+    "HostStarMetallicity": 0,
+    "HostStarTempK": 3584000,
+    "HostStarAgeGyr": ""
+  },
+
+
+
+
+
+
 ]
 
   
-
-
-
 describe('Checking orphone planet ', () => {
   it('Checking if no of orphan planet are the same',  () => {
     const wrapper = shallowMount(HW, {
@@ -205,7 +314,6 @@ describe('Checking orphone planet ', () => {
 
 
 
-
 describe('Checking orphone planet ', () => {
   it('Checking if the  planet with hotest start is found',  () => {
     const wrapper = shallowMount(HW, {
@@ -214,6 +322,27 @@ describe('Checking orphone planet ', () => {
     assert.equal("KOI-1843.03", wrapper.vm.hotStarPlanet.PlanetIdentifier);
   })
 })
+
+describe('Checking orphone planet ', () => {
+  it('Checking if the  planet with hotest start is found',  () => {
+    const wrapper = shallowMount(HW, {
+    });
+    wrapper.vm.getYearRecoard(data);
+    assert.equal(2, wrapper.vm.yearTable.length);
+    let val = wrapper.vm.yearTable.findIndex((year => year.Year == 2012));
+    assert.equal(4, wrapper.vm.yearTable[val].small);
+    assert.equal(1, wrapper.vm.yearTable[val].med);
+    assert.equal(1, wrapper.vm.yearTable[val].large);
+    val = wrapper.vm.yearTable.findIndex((year => year.Year == 2011));
+    assert.equal(3, wrapper.vm.yearTable[val].small);
+    assert.equal(1, wrapper.vm.yearTable[val].med);
+    assert.equal(1, wrapper.vm.yearTable[val].large);
+  })
+})
+
+
+
+
 
 
 
